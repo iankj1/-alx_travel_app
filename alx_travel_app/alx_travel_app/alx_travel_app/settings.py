392178db80ba1926b5354e2b687f980401432ee9
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-07-06 23:32:00 +0300

    Update settings.py

--- alx_travel_app/alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/alx_travel_app/settings.py
@@ -1,7 +1,20 @@
-INSTALLED_APPS = [
-    ...
-    'rest_framework',
-    'corsheaders',
-    'drf_yasg',
-    'listings',
-]
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# MySQL DB config from .env
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default='127.0.0.1'),
+        'PORT': env('DB_PORT', default='3306'),
+    }
+}
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
